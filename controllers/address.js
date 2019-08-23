3b21306bb84849fb02d5648ede7fe87ad4a09e91
Sudhakar Annadurai <asudhakar@live.in>
2019-08-23 21:15:38 +0530

    code refactoring

--- controllers/address.js
+++ controllers/address.js
@@ -1,16 +1,10 @@
 const uuidv1 = require('uuid/v1');
 class Address{
-    constructor(){
-        this.address = [];
-    }
-
-    add(address){
+    constructor(address){
         let addressID = uuidv1();
-        this.address[addressID] = [];
-        this.address[addressID]['address'] = address;
+        this.id = addressID;
+        this.address = address;
     }
-
-
 
 }
 
